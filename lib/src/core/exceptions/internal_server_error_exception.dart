3808378572e654d7b82deae3e682bfa8b7d2af85
Carlos Merchán <carerap@gmail.com>
2025-06-23 17:25:06 +0200

    create internal server error exception class

--- lib/src/core/exceptions/internal_server_error_exception.dart
+++ lib/src/core/exceptions/internal_server_error_exception.dart
@@ -1,8 +1,10 @@
 class InternalServerErrorException implements Exception {
   final String message;
   final int statusCode = 500;
+  final String errorCode = 'INTERNAL_SERVER_ERROR';
+  Object? data;
 
-  InternalServerErrorException(this.message);
+  InternalServerErrorException(this.message, {this.data});
 
   @override
   String toString() => 'InternalServerErrorException: $message';
